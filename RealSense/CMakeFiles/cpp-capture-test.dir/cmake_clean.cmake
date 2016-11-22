167b49bacfacdd36de3454c7701fe1e650d2a9f2
michael5486 <michael5486@gmail.com>
2016-11-22 05:04:23 +0000

    trying to debug LLVM error

---
+++ RealSense/CMakeFiles/cpp-capture-test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp-capture-test.dir/cpp-capture-test.cpp.o"
+  "cpp-capture-test.pdb"
+  "cpp-capture-test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp-capture-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ RealSense/CMakeFiles/cpp-test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp-test.dir/cpp-test.cpp.o"
+  "cpp-test.pdb"
+  "cpp-test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
