e85cbcaac718f2b142af237449e5fea8c3dbb595
Sragvi Tirumala <sragvi.t@gmail.com>
2017-02-14 00:42:56 -0500

    Created circularBuffer class, need to figure out incorporating robot movement into generating an accurate person position vector

--- RealSense/Projects/userId/userId/circlebuffer.h
+++ RealSense/Projects/userId/userId/circlebuffer.h
@@ -2,89 +2,41 @@
 #include <iostream>
 #include <stdlib.h>
 #include "myPoint.h"
-#include <math.h>
 
 #define bufferSize 10
 
-int writePointer = 0;
-int readPointer = 0;
-
-
-location locBuffer[bufferSize]; //creates the buffer
-
-
-location createLocation(double x, double y, double distance) { //creates a new location struct
-	location newLocation;
-	newLocation.x = x;
-	newLocation.y = y;
-	newLocation.distance = distance;
-	return newLocation;
-}
-
-void createBuffer() { //initializes the buffer
-	for (int i = 0; i < bufferSize; i++) {
-		location tempLocation = createLocation(0, 0, 0);
-		locBuffer[i] = tempLocation;
+class circleBuffer {
+private:
+	myPoint locBuffer[bufferSize];
+	int writePointer, readPointer; //writePointer points to next location, readPointer points to current location
+	double personVector[2]; //2D vector in the x-z plane
+public:
+	//default constructor
+	circleBuffer() { 
+		writePointer = 0;
+		readPointer = 0;
+		locBuffer[bufferSize];
 	}
+	//method declaration
+	void printBuffer();
+	void addLocation(myPoint);
+	void updatePersonVector();
+};
 
-}
-
-void printLocation(location toPrint) {
-	printf("x: %.2f y: %.2f distance: %.2f\n", toPrint.x, toPrint.y, toPrint.distance);
-}
-
-void printBuffer() {
+void circleBuffer::printBuffer() {
 	printf("-----------Full Buffer--------\n");
 	for (int i = 0; i < bufferSize; i++) {
-		printLocation(locBuffer[i]);
-	}
-
-}
-
-void increaseReadPtr() {
-	readPointer = (readPointer + 1) % bufferSize; //increases readPointer and wraps around to beginning
-}
-
-void increaseWritePtr() {
-	writePointer = (writePointer + 1) % bufferSize; //increases writePointer and wraps around to beginning
-	readPointer - writePointer; //we want readPointer to stay consistent with readpointer
-}
-
-void decreaseReadPtr() {
-	readPointer = readPointer - 1;
-	if (readPointer == -1) {
-		readPointer = bufferSize;
+		locBuffer[i].printPoint;
 	}
 }
 
-/*may want to have a function here that looks at the last couple of locations and
-use them to decide which side of the screen the user left from*/
+void circleBuffer::addLocation(myPoint newLoc) {
+	locBuffer[writePointer] = newLoc;
+	readPointer = writePointer;
+	writePointer = (writePointer + 1) % bufferSize;
+	updatePersonVector();
+}
 
-void main() {
-
-	/* initialize random seed: */
-	srand(time(NULL));
-
-	int pause;
-
-	location newLocation = createLocation(50, 50, 2);
-	printLocation(newLocation);
-
-	createBuffer();
-	printBuffer();
-
-
-	while (true) {
-		location newLocation = createLocation(rand() % 100, rand() % 100, rand() % 100);
-		locBuffer[writePointer] = newLocation;
-		increaseWritePtr();
-		Sleep(100);
-		printBuffer();
-
-	}
-
-
-	std::cout << "Press any key to end...\n";
-	std::cin >> pause;
-
+void circleBuffer::updatePersonVector(){
+	//how to incorporate robot position into this?
 }
