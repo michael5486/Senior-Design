8a43c53e6acf60fda1b0dac84ee4ae511a0372ec
John Buchholz <johnbuchholz@Johns-MacBook-Pro.local>
2017-04-12 00:39:10 -0400

    motor fixes

--- Motors/readSerialMotorTest/readSerialMotorTest.ino
+++ Motors/readSerialMotorTest/readSerialMotorTest.ino
@@ -60,130 +60,132 @@ void parseByte(int byte) {
 void controlLEDs() {
 
     //clears input to each pin
-    analogWrite(leftA1, 0);
-    analogWrite(leftA2, 0);
-    analogWrite(rightA1, 0);
-    analogWrite(rightA2, 0);
+    analogWrite(leftA1, 255);
+    analogWrite(leftA2, 255);
+    analogWrite(rightA1, 255);
+    analogWrite(rightA2, 255); 
     
 
   switch(leftCommand) {
-    case 0: // Left Wheel Backward Full -- Needs to go CW
-      analogWrite(leftA1, 255);
+    case 0: // Left Wheel Backward Full -- Needs to go CCW
+      analogWrite(leftA1, 0);
       analogWrite(leftA2, 255);
     break;
     case 1: 
-      analogWrite(leftA1, 225);
-      analogWrite(leftA2, 225);
+      analogWrite(leftA1, 30);
+      analogWrite(leftA2, 255);
     break;
     case 2:
-      analogWrite(leftA1, 195);
-      analogWrite(leftA2, 195);
+      analogWrite(leftA1, 60);
+      analogWrite(leftA2, 255);
     break;
     case 3:
-      analogWrite(leftA1, 165);
-      analogWrite(leftA2, 165);
+      analogWrite(leftA1, 90);
+      analogWrite(leftA2, 255);
     break;
     case 4:
-      analogWrite(leftA1, 135);
-      analogWrite(leftA2, 135);
+      analogWrite(leftA1, 120);
+      analogWrite(leftA2, 255);
     break;
     case 5:
-      analogWrite(leftA1, 105);
-      analogWrite(leftA2, 105);
+      analogWrite(leftA1, 150);
+      analogWrite(leftA2, 255);
     break;
-    case 6: // Left Backward 0
-      analogWrite(leftA1, 0);
-      analogWrite(leftA2, 0);
+    case 6: // Left Backward Low
+      analogWrite(leftA1, 180);
+      analogWrite(leftA2, 255);
     break;
     //case 7: NOTHING
       //analogWrite(leftBackwardLEDPin, 0);
     //break;
-    case 8: // Left Wheel Forward 0 -- Needs to go CCW
-      analogWrite(leftA1, 0);
-      analogWrite(leftA2, 0);
+    case 8: // Left Wheel Forward Low -- Needs to go CW
+      analogWrite(leftA1, 180);
+      analogWrite(leftA2, 180);
     break;
     case 9:
-      analogWrite(leftA1, 105);
-      analogWrite(leftA2, 255);
+      analogWrite(leftA1, 150);
+      analogWrite(leftA2, 150);
     break;
     case 10:
-      analogWrite(leftA1, 135);
-      analogWrite(leftA2, 255);
+      analogWrite(leftA1, 120);
+      analogWrite(leftA2, 120);
     break;
     case 11:
-      analogWrite(leftA1, 165);
-      analogWrite(leftA2, 255);
+      analogWrite(leftA1, 90);
+      analogWrite(leftA2, 90);
     break;
     case 12:
-      analogWrite(leftA1, 195);
-      analogWrite(leftA2, 255);
+      analogWrite(leftA1, 60);
+      analogWrite(leftA2, 60);
     break;
     case 13:
-      analogWrite(leftA1, 225);
-      analogWrite(leftA2, 255);
+      analogWrite(leftA1, 30);
+      analogWrite(leftA2, 30);
     break;
     case 14: // Left Forward Full
-      analogWrite(leftA1, 255);
-      analogWrite(leftA2, 255);
+      analogWrite(leftA1, 0);
+      analogWrite(leftA2, 0);
     break;
   }
 
     switch(rightCommand) { // Right Wheel Set
-    case 0: // Right Wheel Backward Full -- Needs to go CCW
-      analogWrite(rightA1, 255);
-      analogWrite(rightA2, 255);
+    case 0: // Right Wheel Backward Full -- Needs to go CW
+      analogWrite(rightA1, 0);
+      analogWrite(rightA2, 0);
     break;
     case 1:
-      analogWrite(rightA1, 225);
-      analogWrite(rightA2, 255);
+      analogWrite(rightA1, 30);
+      analogWrite(rightA2, 30);
     break;
     case 2:
-      analogWrite(rightA1, 195);
-      analogWrite(rightA2, 255);
+      analogWrite(rightA1, 60);
+      analogWrite(rightA2, 60);
     break;
     case 3:
-      analogWrite(rightA1, 165);
-      analogWrite(rightA2, 255);
+      analogWrite(rightA1, 90);
+      analogWrite(rightA2, 90);
     break;
     case 4:
-      analogWrite(rightA1, 135);
-      analogWrite(rightA2, 255);
+      analogWrite(rightA1, 120);
+      analogWrite(rightA2, 120);
     break;
     case 5:
-      analogWrite(rightA1, 105);
-      analogWrite(rightA2, 255);
+      analogWrite(rightA1, 150);
+      analogWrite(rightA2, 150);
     break;
     case 6: // Right Backward 0
-      analogWrite(rightA1, 0);
-      analogWrite(rightA2, 0);
+      analogWrite(rightA1, 180);
+      analogWrite(rightA2, 180);
     break;
-    //case 7: NOTHING
+    case 7: 
       //analogWrite(rightBackwardLEDPin, 0);
-    //break;
-    case 8: // Right Wheel Forward 0 -- Needs to go CW
-      analogWrite(rightA1, 0);
-      analogWrite(rightA2, 0);
+      analogWrite(rightA1, 255);
+      analogWrite(rightA2, 255);
+    break;
+    case 8: // Right Wheel Forward 0 -- Needs to go CCW
+      analogWrite(rightA1, 180);
+      analogWrite(rightA2, 255);
     break;
     case 9:
-      analogWrite(rightA1, 105);
-      analogWrite(rightA2, 105);
+      analogWrite(rightA1, 150);
+      analogWrite(rightA2, 255);
     break;
     case 10:
-      analogWrite(rightA1, 135);
-      analogWrite(rightA2, 135);
+      analogWrite(rightA1, 120);
+      analogWrite(rightA2, 255);
     break;
     case 11:
-      analogWrite(rightA1, 165);
-      analogWrite(rightA2, 165);
+      analogWrite(rightA1, 90);
+      analogWrite(rightA2, 255);
     break;
     case 12:
-      analogWrite(rightA1, 195);
-      analogWrite(rightA2, 195);
+      analogWrite(rightA1, 60);
+      analogWrite(rightA2, 255);
     case 13:
-      analogWrite(rightA1, 225);
-      analogWrite(rightA2, 225);
+      analogWrite(rightA1, 30);
+      analogWrite(rightA2, 255);
     case 14: // Right Forward Full
-      analogWrite(rightA1, 255);
+      analogWrite(rightA1, 0);
       analogWrite(rightA2, 255);
     break;
   }
