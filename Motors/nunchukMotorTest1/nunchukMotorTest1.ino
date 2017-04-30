cbf922bb119c02e83ce6eae126d58de354f21d86
stirumala17 <sragvi.t@gmail.com>
2017-04-30 16:29:07 -0400

    Motor controls for new H bridges

--- Motors/nunchukMotorTest1/nunchukMotorTest1.ino
+++ Motors/nunchukMotorTest1/nunchukMotorTest1.ino
@@ -12,6 +12,15 @@ int leftA2 = 4;
 int rightA1 = 5;
 int rightA2 = 6;
 
+/* Different Set of H-Bridges L6203 */
+/* SET PINS HERE MIKEY */
+int leftB1 = ;
+int leftB2 = ;
+int rightB1 = ;
+int rightB2 = ;
+
+/* ENABLE PIN NEEDS TO BE TURNED ON FOR THESE H BRIDGES TO WORK, IDK WHERE TO IMPLEMENT DIGITAL PINOUT */
+int enabler = ;
 
 /* Function declarations */
 void setMotorCommands(int, int);
@@ -66,68 +75,122 @@ void controlMotors() {
     analogWrite(leftA1, 255);
     analogWrite(leftA2, 255);
     analogWrite(rightA1, 255);
-    analogWrite(rightA2, 255); 
+    analogWrite(rightA2, 255);
+
+     /* For L6203, set inputs 1 and 2 to L for off */
+     analogWrite(leftB1, 0);
+    analogWrite(leftB2, 0);
+    analogWrite(rightB1, 0);
+    analogWrite(rightB2, 0);
     
+/* Input settings for each set of H-bridges given desired motor output */
+/* Need to experiment to determine what speeds correspond to the correct */
 
   switch(leftCommand) {
     case 0: // Left Wheel Backward Full -- Needs to go CCW
       analogWrite(leftA1, 0);
       analogWrite(leftA2, 255);
+      
+      analogWrite(leftB1, 0);
+      analogWrite(leftB2, 255);
     break;
     case 1: 
       analogWrite(leftA1, 30);
       analogWrite(leftA2, 255);
+
+      analogWrite(leftB1, 0);
+      analogWrite(leftB2, 225);
     break;
     case 2:
       analogWrite(leftA1, 60);
       analogWrite(leftA2, 255);
+
+      analogWrite(leftB1, 0);
+      analogWrite(leftB2, 195);
     break;
     case 3:
       analogWrite(leftA1, 90);
       analogWrite(leftA2, 255);
+
+      analogWrite(leftB1, 0);
+      analogWrite(leftB2, 165);
     break;
     case 4:
       analogWrite(leftA1, 120);
       analogWrite(leftA2, 255);
+
+      analogWrite(leftB1, 0);
+      analogWrite(leftB2, 135);
     break;
     case 5:
       analogWrite(leftA1, 150);
       analogWrite(leftA2, 255);
+
+      analogWrite(leftB1, 0);
+      analogWrite(leftB2, 105);
     break;
     case 6: // Left Backward Low
       analogWrite(leftA1, 180);
       analogWrite(leftA2, 255);
+
+      analogWrite(leftB1, 0);
+      analogWrite(leftB2, 75);
     break;
-    //case 7: NOTHING
-      //analogWrite(leftBackwardLEDPin, 0);
+    //case 7: No movement
+      analogWrite(leftA1, 255);
+      analogWrite(leftA2, 255);
+
+      analogWrite(leftB1, 0);
+      analogWrite(leftB2, 0);
     //break;
     case 8: // Left Wheel Forward Low -- Needs to go CW
       analogWrite(leftA1, 180);
       analogWrite(leftA2, 180);
+
+      analogWrite(leftB1, 75);
+      analogWrite(leftB2, 0);
     break;
     case 9:
       analogWrite(leftA1, 150);
       analogWrite(leftA2, 150);
+
+      analogWrite(leftB1, 105);
+      analogWrite(leftB2, 0);
     break;
     case 10:
       analogWrite(leftA1, 120);
       analogWrite(leftA2, 120);
+
+      analogWrite(leftB1, 135);
+      analogWrite(leftB2, 0);
     break;
     case 11:
       analogWrite(leftA1, 90);
       analogWrite(leftA2, 90);
+
+      analogWrite(leftB1, 165);
+      analogWrite(leftB2, 0);
     break;
     case 12:
       analogWrite(leftA1, 60);
       analogWrite(leftA2, 60);
+
+      analogWrite(leftB1, 195);
+      analogWrite(leftB2, 0);
     break;
     case 13:
       analogWrite(leftA1, 30);
       analogWrite(leftA2, 30);
+
+      analogWrite(leftB1, 225);
+      analogWrite(leftB2, 0);
     break;
     case 14: // Left Forward Full
       analogWrite(leftA1, 0);
       analogWrite(leftA2, 0);
+
+      analogWrite(leftB1, 255);
+      analogWrite(leftB2, 0);
     break;
   }
 
@@ -135,61 +198,106 @@ void controlMotors() {
     case 0: // Right Wheel Backward Full -- Needs to go CW
       analogWrite(rightA1, 0);
       analogWrite(rightA2, 0);
+
+      analogWrite(rightB1, 255);
+      analogWrite(rightB2, 0);
     break;
     case 1:
       analogWrite(rightA1, 30);
       analogWrite(rightA2, 30);
+
+      analogWrite(rightB1, 225);
+      analogWrite(rightB2, 0);
     break;
     case 2:
       analogWrite(rightA1, 60);
       analogWrite(rightA2, 60);
+
+      analogWrite(rightB1, 195);
+      analogWrite(rightB2, 0);
     break;
     case 3:
       analogWrite(rightA1, 90);
       analogWrite(rightA2, 90);
+
+      analogWrite(rightB1, 165);
+      analogWrite(rightB2, 0);
     break;
     case 4:
       analogWrite(rightA1, 120);
       analogWrite(rightA2, 120);
+
+      analogWrite(rightB1, 135);
+      analogWrite(rightB2, 0);
     break;
     case 5:
       analogWrite(rightA1, 150);
       analogWrite(rightA2, 150);
+
+      analogWrite(rightB1, 105);
+      analogWrite(rightB2, 0);
     break;
     case 6: // Right Backward 0
       analogWrite(rightA1, 180);
       analogWrite(rightA2, 180);
+
+      analogWrite(rightB1, 75);
+      analogWrite(rightB2, 0);
     break;
     case 7: 
       //analogWrite(rightBackwardLEDPin, 0);
       analogWrite(rightA1, 255);
       analogWrite(rightA2, 255);
+
+      analogWrite(rightB1, 0);
+      analogWrite(rightB2, 0);
     break;
     case 8: // Right Wheel Forward 0 -- Needs to go CCW
       analogWrite(rightA1, 180);
       analogWrite(rightA2, 255);
+
+      analogWrite(rightB1, 0);
+      analogWrite(rightB2, 75);
     break;
     case 9:
       analogWrite(rightA1, 150);
       analogWrite(rightA2, 255);
+
+      analogWrite(rightB1, 0);
+      analogWrite(rightB2, 105);
     break;
     case 10:
       analogWrite(rightA1, 120);
       analogWrite(rightA2, 255);
+
+      analogWrite(rightB1, 0);
+      analogWrite(rightB2, 135);
     break;
     case 11:
       analogWrite(rightA1, 90);
       analogWrite(rightA2, 255);
+
+      analogWrite(rightB1, 0);
+      analogWrite(rightB2, 165);
     break;
     case 12:
       analogWrite(rightA1, 60);
       analogWrite(rightA2, 255);
+
+      analogWrite(rightB1, 0);
+      analogWrite(rightB2, 195);
     case 13:
       analogWrite(rightA1, 30);
       analogWrite(rightA2, 255);
+
+      analogWrite(rightB1, 0);
+      analogWrite(rightB2, 225);
     case 14: // Right Forward Full
       analogWrite(rightA1, 0);
       analogWrite(rightA2, 255);
+
+      analogWrite(rightB1, 0);
+      analogWrite(rightB2, 255);
     break;
   }
 }  
